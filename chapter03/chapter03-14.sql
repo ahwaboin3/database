2f71959b44da96f54a346b36e0d5beca88c79acb
devgiants75 <devgiants75@naver.com>
2024-04-24 09:21:55 +0900

    Update chapter03-14.sql

--- chapter03/chapter03-14.sql
+++ chapter03/chapter03-14.sql
@@ -88,6 +88,21 @@ select * from reservation;
 -- 4. 2020년 9월 1일에 가장 많은 고객이 관람한 영화를 보이시오.
 select * from reservation;
 
+-- DML
+-- 각 테이블에 데이터를 삽입하는 insert문들을 하나씩 보이시오.
+-- 상영관에 데이터 삽입
+insert into cinema values(1,11,'범죄의도시4',15000,7);
+-- 고객 테이블에 데이터 삽입
+insert into movie_customer 
+values(22,'김고객','마포');
+-- 예약  테이블에 데이터 삽입
+insert into reservation values(1,10,22,7,'2024-4-24');
+-- 극장 테이블에 데이터 삽입
+insert into theater values(33,'롯데시네마','서면본점');
+
+-- 2. 영화의 가격을 10% 인상하시오. (x*1.1)
+update cinema set movieprice=movieprice*1.1;
+
 
 
 
